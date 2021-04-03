2f7df23172c0bea075fe5f7bea1f540fc279e115
Kopynets Artem <bachik85@gmail.com>
2021-04-03 21:13:31 +0300

    Initial commit

---
+++ build/precache-manifest.dfd145cc21b8c56930f3add3f1595221.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a434920f12d6659eec2a99229207b151",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a9f5843bf21cec7f3ea",
+    "url": "/static/css/main.c058a1bd.chunk.css"
+  },
+  {
+    "revision": "276fd98e286c1483313f",
+    "url": "/static/js/2.ed9f79da.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/static/js/2.ed9f79da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a9f5843bf21cec7f3ea",
+    "url": "/static/js/main.7b54f37e.chunk.js"
+  },
+  {
+    "revision": "336411e562eb08a03ce2",
+    "url": "/static/js/runtime-main.75fe034e.js"
+  },
+  {
+    "revision": "6995fcc9263483d254622308d6c275f1",
+    "url": "/static/media/fonmil.6995fcc9.png"
+  }
+]);
